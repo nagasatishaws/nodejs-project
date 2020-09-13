16a35d4413116e64eb465e96223c4951f92e0bbb
nagasatishaws <64299813+nagasatishaws@users.noreply.github.com>
2020-09-13 17:39:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ var timestamp = new Date().getTime();
 /*
     Fire the server online
 */
-app.set('port', (process.env.PORT || 7000));
+app.set('port', (process.env.PORT || 1000));
 app.listen(app.get('port'), function() {
 	console.log('Server started on port '+ app.get('port'));
 });
